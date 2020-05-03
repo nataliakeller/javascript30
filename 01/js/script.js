46355e0c06c452e97fad7758f866ddc3d3a7283f
nataliakeller <natalia-farah@uergs.edu.br>
2020-05-03 22:49:21 +0200

    javascript ok

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,19 +1,16 @@
-var boom = new Audio('../sounds/boom.wav')
-var clap = new Audio('../sounds/clap.wav')
-var hihat = new Audio('../sounds/hihat.wav')
-var kick = new Audio('../sounds/kick.wav')
-var openhat = new Audio('../sounds/openhat.wav')
-var ride = new Audio('../sounds/ride.wav')
-var snare = new Audio('../sounds/snare.wav')
-var tink = new Audio('../sounds/tink.wav')
-var tom = new Audio('../sounds/tom.wav')
-
+var boom = new Audio('./sounds/boom.wav')
+var clap = new Audio('./sounds/clap.wav')
+var hihat = new Audio('./sounds/hihat.wav')
+var kick = new Audio('./sounds/kick.wav')
+var openhat = new Audio('./sounds/openhat.wav')
+var ride = new Audio('./sounds/ride.wav')
+var snare = new Audio('./sounds/snare.wav')
+var tink = new Audio('./sounds/tink.wav')
+var tom = new Audio('./sounds/tom.wav')
 
 
 function keyCode(event) {
-    
-    
-    
+
     var x = event.keyCode;
     if (x == 65) {
         document.getElementById('msg').innerHTML = 'letra a'
