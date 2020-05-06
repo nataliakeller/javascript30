b4eb3390a002f31df7bb0271821b6d96576d6c56
nataliakeller <natalia-farah@uergs.edu.br>
2020-05-06 20:46:18 +0200

    drum kit done, added functions to remove transition and switch case added

--- 01/js/script.js
+++ 01/js/script.js
@@ -8,36 +8,75 @@ var snare = new Audio('./sounds/snare.wav')
 var tink = new Audio('./sounds/tink.wav')
 var tom = new Audio('./sounds/tom.wav')
 
-
-function keyCode(event) {
-
+function tocar(event) {
     var x = event.keyCode;
-    if (x == 65) {
-        document.getElementById('msg').innerHTML = 'letra a'
-        boom.play()
-    } else if (x == 83) {
-        document.getElementById('msg').innerHTML = 'letra s'
-        clap.play()
-    } else if (x == 68) {
-        document.getElementById('msg').innerHTML = 'letra d'
-        hihat.play()
-    } else if (x == 70) {
-        document.getElementById('msg').innerHTML = 'letra f'
-        kick.play()
-    } else if (x == 71) {
-        document.getElementById('msg').innerHTML = 'letra g'
-        openhat.play()
-    } else if (x == 72) {
-        document.getElementById('msg').innerHTML = 'letra h'
-        ride.play()
-    } else if (x == 74) {
-        document.getElementById('msg').innerHTML = 'letra j'
-        snare.play()
-    } else if (x == 75) {
-        document.getElementById('msg').innerHTML = 'letra k'
-        tink.play()
-    } else if (x == 76) {
-        document.getElementById('msg').innerHTML = 'letra l'
-        tom.play()
+    
+    switch(x) {
+        case 65:
+            boom.currentTime = 0;
+            boom.play()
+            var playing = document.getElementById('boom')
+            playing.classList.add('playing')
+            break;
+        case 83:
+            clap.currentTime = 0;
+            clap.play()
+            var playing = document.getElementById('clap')
+            playing.classList.add('playing')
+            break;
+        case 68:
+            hihat.currentTime = 0;
+            hihat.play()
+            var playing = document.getElementById('hihat')
+            playing.classList.add('playing')
+            break;
+        case 70:
+            kick.currentTime = 0;
+            kick.play()
+            var playing = document.getElementById('kick')
+            playing.classList.add('playing')
+            break;
+        case 71:
+            openhat.currentTime = 0;
+            openhat.play()
+            var playing = document.getElementById('openhat')
+            playing.classList.add('playing')
+            break;
+        case 72:
+            ride.currentTime = 0;
+            ride.play()
+            var playing = document.getElementById('ride')
+            playing.classList.add('playing')
+            break;
+        case 74:
+            snare.currentTime = 0;
+            snare.play()    
+            var playing = document.getElementById('snare')
+            playing.classList.add('playing')
+            break;
+        case 75:
+            tink.currentTime = 0;
+            tink.play()
+            var playing = document.getElementById('tink')
+            playing.classList.add('playing')
+            break;
+        case 76:
+            tom.currentTime = 0;
+            tom.play()    
+            var playing = document.getElementById('tom')
+            playing.classList.add('playing')
+            break;
     }
+
+
 }
+
+const keys = Array.from(document.querySelectorAll('.input'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  //seleciona em um array cada div input e adiciona um event listener do transition end e remove a transição
+  //quando ela termina
+
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  } // função para remover a classe playing
